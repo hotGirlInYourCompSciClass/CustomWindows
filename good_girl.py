c46a25b2b224294447ce6e63aa4132ff28974c58
hotGirlInYourCompSciClass <131593373+hotGirlInYourCompSciClass@users.noreply.github.com>
2025-05-22 08:27:07 +0000

    good girl <3

--- good_girl.py
+++ good_girl.py
@@ -1,15 +1,18 @@
 import tkinter as tk
 
-def run_good_girl():
-    winWidth = 300
-    winHeight = 300
+def run_good_girl(master_root):
+    winWidth = 900
+    winHeight = 150
 
     m = tk.Toplevel(master_root)
     m.title("good girl!!")
-    m.geometry(f"winWidthxwinHeight")
+    m.geometry(f"{winWidth}x{winHeight}+{int((master_root.winfo_screenwidth() - winWidth) / 2)}+{int((master_root.winfo_screenheight() - winHeight) / 2)}")
     m.configure(bg="orchid3")
     m.attributes("-topmost", True)
+    m.overrideredirect(True)
 
+    lbl = tk.Label(m, text="Good girl!!!!!!", font=("Arial", 100, "bold"), bg="orchid3", fg="black")
+    lbl.pack()
 
 root = tk.Tk()
 root.withdraw()
